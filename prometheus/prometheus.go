62aaed480ed6d7fdb9b1675f2199ff0ce0a32efb
Carlos de Paula <me@carlosedp.com>
2019-09-11 10:22:18 -0300

    Add Prometheus metrics middleware

---
+++ prometheus/prometheus.go
@@ -0,0 +1,422 @@
+/*
+Package prometheus provides middleware to add Prometheus metrics.
+
+Example:
+```
+	package main
+	import (
+		"github.com/labstack/echo/v4"
+		"github.com/labstack/echo-contrib/prometheus"
+	)
+	func main() {
+		e := echo.New()
+		// Enable metrics middleware
+		p := prometheus.NewPrometheus("echo")
+		p.Use(e)
+
+		e.Logger.Fatal(e.Start(":1323"))
+	}
+```
+*/
+package prometheus
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+)
+
+var defaultMetricPath = "/metrics"
+
+// Standard default metrics
+//	counter, counter_vec, gauge, gauge_vec,
+//	histogram, histogram_vec, summary, summary_vec
+var reqCnt = &Metric{
+	ID:          "reqCnt",
+	Name:        "requests_total",
+	Description: "How many HTTP requests processed, partitioned by status code and HTTP method.",
+	Type:        "counter_vec",
+	Args:        []string{"code", "method", "host", "url"}}
+
+var reqDur = &Metric{
+	ID:          "reqDur",
+	Name:        "request_duration_seconds",
+	Description: "The HTTP request latencies in seconds.",
+	Type:        "summary"}
+
+var resSz = &Metric{
+	ID:          "resSz",
+	Name:        "response_size_bytes",
+	Description: "The HTTP response sizes in bytes.",
+	Type:        "summary"}
+
+var reqSz = &Metric{
+	ID:          "reqSz",
+	Name:        "request_size_bytes",
+	Description: "The HTTP request sizes in bytes.",
+	Type:        "summary"}
+
+var standardMetrics = []*Metric{
+	reqCnt,
+	reqDur,
+	resSz,
+	reqSz,
+}
+
+/*
+RequestCounterURLLabelMappingFn is a function which can be supplied to the middleware to control
+the cardinality of the request counter's "url" label, which might be required in some contexts.
+For instance, if for a "/customer/:name" route you don't want to generate a time series for every
+possible customer name, you could use this function:
+
+func(c echo.Context) string {
+	url := c.Request.URL.Path
+	for _, p := range c.Params {
+		if p.Key == "name" {
+			url = strings.Replace(url, p.Value, ":name", 1)
+			break
+		}
+	}
+	return url
+}
+
+which would map "/customer/alice" and "/customer/bob" to their template "/customer/:name".
+*/
+type RequestCounterURLLabelMappingFn func(c echo.Context) string
+
+// Metric is a definition for the name, description, type, ID, and
+// prometheus.Collector type (i.e. CounterVec, Summary, etc) of each metric
+type Metric struct {
+	MetricCollector prometheus.Collector
+	ID              string
+	Name            string
+	Description     string
+	Type            string
+	Args            []string
+}
+
+// Prometheus contains the metrics gathered by the instance and its path
+type Prometheus struct {
+	reqCnt               *prometheus.CounterVec
+	reqDur, reqSz, resSz prometheus.Summary
+	router               *echo.Echo
+	listenAddress        string
+	Ppg                  PrometheusPushGateway
+
+	MetricsList []*Metric
+	MetricsPath string
+
+	ReqCntURLLabelMappingFn RequestCounterURLLabelMappingFn
+
+	// Context string to use as a prometheus URL label
+	URLLabelFromContext string
+}
+
+// PrometheusPushGateway contains the configuration for pushing to a Prometheus pushgateway (optional)
+type PrometheusPushGateway struct {
+
+	// Push interval in seconds
+	PushIntervalSeconds time.Duration
+
+	// Push Gateway URL in format http://domain:port
+	// where JOBNAME can be any string of your choice
+	PushGatewayURL string
+
+	// Local metrics URL where metrics are fetched from, this could be ommited in the future
+	// if implemented using prometheus common/expfmt instead
+	MetricsURL string
+
+	// pushgateway job name, defaults to "echo"
+	Job string
+}
+
+// NewPrometheus generates a new set of metrics with a certain subsystem name
+func NewPrometheus(subsystem string, customMetricsList ...[]*Metric) *Prometheus {
+	var metricsList []*Metric
+
+	if len(customMetricsList) > 1 {
+		panic("Too many args. NewPrometheus( string, <optional []*Metric> ).")
+	} else if len(customMetricsList) == 1 {
+		metricsList = customMetricsList[0]
+	}
+
+	for _, metric := range standardMetrics {
+		metricsList = append(metricsList, metric)
+	}
+
+	p := &Prometheus{
+		MetricsList: metricsList,
+		MetricsPath: defaultMetricPath,
+		ReqCntURLLabelMappingFn: func(c echo.Context) string {
+			return c.Path() // i.e. by default do nothing, i.e. return URL as is
+		},
+	}
+
+	p.registerMetrics(subsystem)
+
+	return p
+}
+
+// SetPushGateway sends metrics to a remote pushgateway exposed on pushGatewayURL
+// every pushIntervalSeconds. Metrics are fetched from metricsURL
+func (p *Prometheus) SetPushGateway(pushGatewayURL, metricsURL string, pushIntervalSeconds time.Duration) {
+	p.Ppg.PushGatewayURL = pushGatewayURL
+	p.Ppg.MetricsURL = metricsURL
+	p.Ppg.PushIntervalSeconds = pushIntervalSeconds
+	p.startPushTicker()
+}
+
+// SetPushGatewayJob job name, defaults to "echo"
+func (p *Prometheus) SetPushGatewayJob(j string) {
+	p.Ppg.Job = j
+}
+
+// SetListenAddress for exposing metrics on address. If not set, it will be exposed at the
+// same address of the echo engine that is being used
+// func (p *Prometheus) SetListenAddress(address string) {
+// 	p.listenAddress = address
+// 	if p.listenAddress != "" {
+// 		p.router = echo.Echo().Router()
+// 	}
+// }
+
+// SetListenAddressWithRouter for using a separate router to expose metrics. (this keeps things like GET /metrics out of
+// your content's access log).
+// func (p *Prometheus) SetListenAddressWithRouter(listenAddress string, r *echo.Echo) {
+// 	p.listenAddress = listenAddress
+// 	if len(p.listenAddress) > 0 {
+// 		p.router = r
+// 	}
+// }
+
+// SetMetricsPath set metrics paths
+func (p *Prometheus) SetMetricsPath(e *echo.Echo) {
+
+	if p.listenAddress != "" {
+		p.router.GET(p.MetricsPath, prometheusHandler())
+		p.runServer()
+	} else {
+		e.GET(p.MetricsPath, prometheusHandler())
+	}
+}
+
+func (p *Prometheus) runServer() {
+	if p.listenAddress != "" {
+		go p.router.Start(p.listenAddress)
+	}
+}
+
+func (p *Prometheus) getMetrics() []byte {
+	response, err := http.Get(p.Ppg.MetricsURL)
+	if err != nil {
+		log.WithError(err).Errorln("Error getting metrics")
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	return body
+}
+
+func (p *Prometheus) getPushGatewayURL() string {
+	h, _ := os.Hostname()
+	if p.Ppg.Job == "" {
+		p.Ppg.Job = "echo"
+	}
+	return p.Ppg.PushGatewayURL + "/metrics/job/" + p.Ppg.Job + "/instance/" + h
+}
+
+func (p *Prometheus) sendMetricsToPushGateway(metrics []byte) {
+	req, err := http.NewRequest("POST", p.getPushGatewayURL(), bytes.NewBuffer(metrics))
+	client := &http.Client{}
+	if _, err = client.Do(req); err != nil {
+		log.WithError(err).Errorln("Error sending to push gateway")
+	}
+}
+
+func (p *Prometheus) startPushTicker() {
+	ticker := time.NewTicker(time.Second * p.Ppg.PushIntervalSeconds)
+	go func() {
+		for range ticker.C {
+			p.sendMetricsToPushGateway(p.getMetrics())
+		}
+	}()
+}
+
+// NewMetric associates prometheus.Collector based on Metric.Type
+func NewMetric(m *Metric, subsystem string) prometheus.Collector {
+	var metric prometheus.Collector
+	switch m.Type {
+	case "counter_vec":
+		metric = prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+			m.Args,
+		)
+	case "counter":
+		metric = prometheus.NewCounter(
+			prometheus.CounterOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+		)
+	case "gauge_vec":
+		metric = prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+			m.Args,
+		)
+	case "gauge":
+		metric = prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+		)
+	case "histogram_vec":
+		metric = prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+			m.Args,
+		)
+	case "histogram":
+		metric = prometheus.NewHistogram(
+			prometheus.HistogramOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+		)
+	case "summary_vec":
+		metric = prometheus.NewSummaryVec(
+			prometheus.SummaryOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+			m.Args,
+		)
+	case "summary":
+		metric = prometheus.NewSummary(
+			prometheus.SummaryOpts{
+				Subsystem: subsystem,
+				Name:      m.Name,
+				Help:      m.Description,
+			},
+		)
+	}
+	return metric
+}
+
+func (p *Prometheus) registerMetrics(subsystem string) {
+
+	for _, metricDef := range p.MetricsList {
+		metric := NewMetric(metricDef, subsystem)
+		if err := prometheus.Register(metric); err != nil {
+			log.WithError(err).Errorf("%s could not be registered in Prometheus", metricDef.Name)
+		}
+		switch metricDef {
+		case reqCnt:
+			p.reqCnt = metric.(*prometheus.CounterVec)
+		case reqDur:
+			p.reqDur = metric.(prometheus.Summary)
+		case resSz:
+			p.resSz = metric.(prometheus.Summary)
+		case reqSz:
+			p.reqSz = metric.(prometheus.Summary)
+		}
+		metricDef.MetricCollector = metric
+	}
+}
+
+// Use adds the middleware to the Echo engine.
+func (p *Prometheus) Use(e *echo.Echo) {
+	e.Use(p.HandlerFunc)
+	p.SetMetricsPath(e)
+}
+
+// HandlerFunc defines handler function for middleware
+func (p *Prometheus) HandlerFunc(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if c.Path() == p.MetricsPath {
+			next(c)
+			return nil
+		}
+
+		start := time.Now()
+		reqSz := computeApproximateRequestSize(c.Request())
+
+		next(c)
+
+		status := strconv.Itoa(c.Response().Status)
+
+		elapsed := float64(time.Since(start)) / float64(time.Second)
+		resSz := float64(c.Response().Size)
+
+		p.reqDur.Observe(elapsed)
+		url := p.ReqCntURLLabelMappingFn(c)
+
+		if len(p.URLLabelFromContext) > 0 {
+			u := c.Get(p.URLLabelFromContext)
+			if u == nil {
+				u = "unknown"
+			}
+			url = u.(string)
+		}
+
+		p.reqCnt.WithLabelValues(status, c.Request().Method, c.Request().Host, url).Inc()
+		p.reqSz.Observe(float64(reqSz))
+		p.resSz.Observe(resSz)
+		return nil
+	}
+}
+
+func prometheusHandler() echo.HandlerFunc {
+	h := promhttp.Handler()
+	return func(c echo.Context) error {
+		h.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
+
+func computeApproximateRequestSize(r *http.Request) int {
+	s := 0
+	if r.URL != nil {
+		s = len(r.URL.Path)
+	}
+
+	s += len(r.Method)
+	s += len(r.Proto)
+	for name, values := range r.Header {
+		s += len(name)
+		for _, value := range values {
+			s += len(value)
+		}
+	}
+	s += len(r.Host)
+
+	// N.B. r.Form and r.MultipartForm are assumed to be included in r.URL.
+
+	if r.ContentLength != -1 {
+		s += int(r.ContentLength)
+	}
+	return s
+}
