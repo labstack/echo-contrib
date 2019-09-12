0bfc7e76da72c23c29f82e082f81b0c27ebff0f1
Carlos de Paula <me@carlosedp.com>
2019-09-11 21:33:59 -0300

    Fix example
    Signed-off-by: Carlos de Paula <me@carlosedp.com>

--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -3,19 +3,19 @@ Package prometheus provides middleware to add Prometheus metrics.
 
 Example:
 ```
-	package main
-	import (
-		"github.com/labstack/echo/v4"
-		"github.com/labstack/echo-contrib/prometheus"
-	)
-	func main() {
-		e := echo.New()
-		// Enable metrics middleware
-		p := prometheus.NewPrometheus("echo", nil)
-		p.Use(e)
+package main
+import (
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo-contrib/prometheus"
+)
+func main() {
+    e := echo.New()
+    // Enable metrics middleware
+    p := prometheus.NewPrometheus("echo", nil)
+    p.Use(e)
 
-		e.Logger.Fatal(e.Start(":1323"))
-	}
+    e.Logger.Fatal(e.Start(":1323"))
+}
 ```
 */
 package prometheus
@@ -374,8 +374,6 @@ func (p *Prometheus) HandlerFunc(next echo.HandlerFunc) echo.HandlerFunc {
 
 		start := time.Now()
 		reqSz := computeApproximateRequestSize(c.Request())
-
-		next(c)
 
 		status := strconv.Itoa(c.Response().Status)
 
